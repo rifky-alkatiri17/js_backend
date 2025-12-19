4437b2c4ac87d37f188257ef7c6778ceb002bb44
Rifki Alkatiri <rifkyalkatiri17@gmail.com>
2025-12-19 21:46:40 +0800

    update folder

--- data_asn_pdi.php
+++ data_asn_pdi.php
@@ -1,5 +1,5 @@
 <?php  
-$conn = mysqli_connect('localhost','root', '', 'db_pns');
+$conn = mysqli_connect('localhost','root', '', 'db_bkppd');
 $select = mysqli_query($conn, 'SELECT * FROM tb_pdi');
 $rows = [];
 while($row = mysqli_fetch_assoc($select)){
